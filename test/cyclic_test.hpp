d0732ab3af1e87b10febc37e498298eca5ff6d3c
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-08-04 12:23:54 +0900

    test: Add IsConstexpr test for length of cyclic

--- test/cyclic_test.hpp
+++ test/cyclic_test.hpp
@@ -13,17 +13,17 @@ TEST_CASE("Vcb")
         CHECK(IsSame<Element_t<decltype(vcb)>, int>::value == true);
         CHECK(vcb.size() == 3);
         CHECK(length(vcb) == 3);
+        CHECK(IsConstexpr<decltype(length(vcb))>::value == false);
         CHECK(vcb.is_empty() == false);
+
         vcb.push_back(1);
         vcb.push_back(2);
         vcb.push_back(3);
         vcb.push_back(4);
+
         CHECK(vcb[0] == 2);
         CHECK(vcb[1] == 3);
         CHECK(vcb[2] == 4);
-
-
-
     }
 }
 
@@ -36,21 +36,28 @@ TEST_CASE("Vcq")
         CHECK(vcq.size() == 0);
         CHECK(vcq.is_empty() == true);
         CHECK(length(vcq) == 0);
+        CHECK(IsConstexpr<decltype(length(vcq))>::value == false);
+
         vcq.push_back(1);
+
         CHECK(vcq.size() == 1);
         CHECK(vcq.is_empty() == false);
         CHECK(vcq.pop_front() == 1);
+
         vcq.push_back(2);
         vcq.push_back(3);
         vcq.push_back(4);
+
         CHECK(vcq.size() == 3);
         CHECK(vcq.pop_front() == 2);
         CHECK(vcq.pop_front() == 3);
         CHECK(vcq.pop_front() == 4);
+
         vcq.push_back(5);
         vcq.push_back(6);
         vcq.push_back(7);
         vcq.push_back(8);
+
         CHECK(vcq.size() == 3);
         CHECK(vcq[0] == 6);
         CHECK(vcq[1] == 7);
