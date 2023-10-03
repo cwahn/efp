4a5584732e02069a8f29a2743f735dc41561a854
paleahn <paleahn@gmail.com>
2023-10-03 14:50:28 +0900

    fix: Remove zero seized array, add missing var

--- test/test_common.hpp
+++ test/test_common.hpp
@@ -7,38 +7,32 @@
 
 using namespace efp;
 
-const Array<double, 0> array_0{};
+const double M_PI = 3.14159265358979323846;
+
 const Array<double, 3> array_3{1., 2., 3.};
 const Array<double, 5> array_5{1., 2., 3., 4., 5.};
 
-const ArrVec<double, 0> arrvec_0{};
 const ArrVec<double, 3> arrvec_3{1., 2., 3.};
 const ArrVec<double, 5> arrvec_5{1., 2., 3., 4., 5.};
 
-const Vector<double> vector_0{};
 const Vector<double> vector_3{1., 2., 3.};
 const Vector<double> vector_5{1., 2., 3., 4., 5.};
 
-const ArrayView<const double, 0> array_view_0{data(array_0)};
 const ArrayView<const double, 3> array_view_3{data(array_3)};
 const ArrayView<const double, 5> array_view_5{data(array_5)};
 
-const ArrVecView<const double, 0> arrvec_view_0{data(vector_0), 0};
 const ArrVecView<const double, 3> arrvec_view_3{data(vector_3), 3};
 const ArrVecView<const double, 5> arrvec_view_5{data(vector_5), 5};
 
-const VectorView<const double> vector_view_0{data(vector_0), 0, 0};
 const VectorView<const double> vector_view_3{data(vector_3), 3, 3};
 const VectorView<const double> vector_view_5{data(vector_5), 5, 5};
 
 const double c_array_3[3] = {1., 2., 3.};
 const double c_array_5[5] = {1., 2., 3., 4., 5.};
 
-const std::array<double, 0> stl_array_0{};
 const std::array<double, 3> stl_array_3{1., 2., 3.};
 const std::array<double, 5> stl_array_5{1., 2., 3., 4., 5.};
 
-const std::vector<double> stl_vector_0{};
 const std::vector<double> stl_vector_3{1., 2., 3.};
 const std::vector<double> stl_vector_5{1., 2., 3., 4., 5.};
 
