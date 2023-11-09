554fc02c3c07350d2ad5d464f9fa745cc566f7bb
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-11-10 02:10:07 +0900

    chore: Draft crtp string

--- include/string.hpp
+++ include/string.hpp
@@ -49,6 +49,24 @@ namespace efp
         CString &operator=(const CString &) = delete;
     };
 
+    // template <int ct_length, int ct_capacity>
+    // class Str : public Sequence<char, ct_length, ct_capacity>
+    // {
+    //     // String(const char *c_str)
+    //     // {
+    //     //     for (size_t i = 0; c_str[i] != '\0'; ++i)
+    //     //     {
+    //     //         push_back(c_str[i]);
+    //     //     }
+    //     // }
+    // };
+
+    // template <typename Derived, typename = EnableIf<
+    //                                 IsSame<RemoveReference<Element<Derived>>, char>::value, void>>
+    // using Str = StringBase<Derived>;
+
+    // using String = StringBase<>;
+
     class String : public Vector<char>
     {
     public:
@@ -64,32 +82,14 @@ namespace efp
 
         ~String() {}
 
-        String &operator+=(const String &other)
-        {
-            for (int i = 0; i < other.size(); ++i)
-            {
-                push_back(other[i]);
-            }
-            return *this;
-        }
-
-        // Will return smart pointer of maybe owning the data if the buffer is full and can't append "/0" at the end
         const CString c_str() const
         {
             const int size_ = size();
-
             char *extended_buffer = new char[size_ + 1];
             memcpy(extended_buffer, data(), size_);
             extended_buffer[size_] = '\0';
             return CString(extended_buffer); // Mark for deletion.
         }
-
-        bool operator==(const String &other) const
-        {
-            return static_cast<const Vector<char> &>(*this) == static_cast<const Vector<char> &>(other);
-        }
-
-        // Other string-specific functions...
     };
 
 };
