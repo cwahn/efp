554482db1ab811494a6e070cbcb0ec2d0113a5b4
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-11-12 20:54:28 +0900

    refactor: Make String as alias of Sequence<char, dyn, dyn>
    Now String is by all mean sequence.
    Could be returned from HoFs.

--- include/string.hpp
+++ include/string.hpp
@@ -52,14 +52,56 @@ namespace efp
         CString &operator=(const CString &) = delete;
     };
 
-    // String
-
-    class String : public Vector<char>
+    template <>
+    class Sequence<char, dyn, dyn>
+        : public SequenceBase<Sequence<char, dyn, dyn>>
     {
     public:
-        String() : Vector<char>() {}
+        using Element = char;
+        static constexpr int ct_len = dyn;
+        static constexpr int ct_cap = dyn;
 
-        String(const char *c_str)
+        static_assert(ct_len >= -1, "ct_length must greater or equal than -1.");
+        static_assert(ct_cap >= -1, "ct_capacity must greater or equal than -1.");
+
+        Sequence() : data_{nullptr}, size_{0}, capacity_{0} {}
+
+        Sequence(const Sequence &other)
+            : data_{nullptr}, size_{0}, capacity_{0}
+        {
+            if (other.data_)
+            {
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+                data_ = new char[capacity_];
+
+                // memcpy(data_, other.data_, sizeof(char) * size_);
+                for (int i = 0; i < other.size_; ++i)
+                {
+                    new (&data_[i]) char(other.data_[i]);
+                }
+            }
+        }
+
+        Sequence(Sequence &&other)
+            : data_{other.data_}, size_{other.size_}, capacity_{other.capacity_}
+        {
+            other.data_ = nullptr;
+        }
+
+        // template <typename... Args>
+        // Sequence(const Args &...args)
+        //     : data_{new char[sizeof...(args)]},
+        //       capacity_(sizeof...(args)),
+        //       size_(sizeof...(args))
+        // {
+        //     int i = 0;
+        //     for (auto arg : std::initializer_list<char>{args...})
+        //         data_[i++] = arg;
+        // }
+
+        Sequence(const char *c_str)
+            : data_{nullptr}, size_{0}, capacity_{0}
         {
             for (size_t i = 0; c_str[i] != '\0'; ++i)
             {
@@ -67,7 +109,184 @@ namespace efp
             }
         }
 
-        ~String() {}
+        ~Sequence()
+        {
+            if (data_)
+            {
+                delete[] data_;
+                data_ = nullptr;
+            }
+        }
+
+        // todo copy_and_swap
+        Sequence &operator=(const Sequence &other) noexcept
+        {
+            if (this != &other)
+            {
+                char *newData = nullptr;
+                if (other.size_ > 0)
+                {
+                    newData = new char[other.capacity_];
+
+                    for (int i = 0; i < other.size_; ++i)
+                    {
+                        newData[i] = other.data_[i];
+                    }
+                }
+
+                data_ = newData;
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+            }
+            return *this;
+        }
+
+        Sequence &operator=(Sequence &&other) noexcept
+        {
+            if (this != &other)
+            {
+                delete[] data_;
+
+                data_ = other.data_;
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+
+                other.data_ = nullptr;
+                other.size_ = 0;
+                other.capacity_ = 0;
+            }
+            return *this;
+        }
+
+        Sequence assign_impl(const Sequence &other)
+        {
+            if (this != &other)
+            {
+                char *newData = nullptr;
+                if (other.size_ > 0)
+                {
+                    newData = new char[other.capacity_];
+
+                    for (int i = 0; i < other.size_; ++i)
+                    {
+                        newData[i] = other.data_[i];
+                    }
+                }
+
+                data_ = newData;
+                size_ = other.size_;
+                capacity_ = other.capacity_;
+            }
+            return *this;
+        }
+
+        char &operator[](int index)
+        {
+            return data_[index];
+        }
+
+        const char &operator[](int index) const
+        {
+            return data_[index];
+        }
+
+        bool operator==(const Sequence &other) const
+        {
+            if (size_ != other.size_)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < size_; ++i)
+            {
+                if (data_[i] != other.data_[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        int size() const
+        {
+            return size_;
+        }
+
+        int capacity() const
+        {
+            return capacity_;
+        }
+
+        void resize(int length)
+        {
+            if (length < 0)
+            {
+                abort();
+            }
+
+            if (length > capacity_)
+            {
+                reserve(length);
+            }
+
+            size_ = length;
+        }
+
+        void reserve(int new_capacity)
+        {
+            if (new_capacity > capacity_)
+            {
+                char *new_data = new char[new_capacity];
+
+                for (int i = 0; i < size_; ++i)
+                {
+                    new (&new_data[i]) char(efp::move(data_[i]));
+                }
+
+                delete[] data_;
+
+                data_ = new_data;
+                capacity_ = new_capacity;
+            }
+        }
+
+        void push_back(const char &value)
+        {
+            if (size_ >= capacity_)
+            {
+                reserve(capacity_ == 0 ? 1 : 2 * capacity_);
+            }
+
+            new (&data_[size_]) char{value};
+            ++size_;
+        }
+
+        void push_back(char &&value)
+        {
+            if (size_ >= capacity_)
+            {
+                reserve(capacity_ == 0 ? 1 : 2 * capacity_);
+            }
+
+            new (&data_[size_]) char(efp::move(value));
+            ++size_;
+        }
+
+        void erase(int index)
+        {
+            if (index < 0 || index >= size_)
+            {
+                abort();
+            }
+
+            for (int i = index; i < size_ - 1; ++i)
+            {
+                new (&data_[i]) char(efp::move(data_[i + 1]));
+            }
+
+            --size_;
+        }
 
         const CString c_str() const
         {
@@ -83,13 +302,55 @@ namespace efp
         //     return efp::append(*this, string);
         // }
 
-        void append_mut(const String &string)
+        void append_mut(const Sequence &string)
         {
             for_each([&](char c)
                      { push_back(c); },
                      string);
         }
+
+        const char *data() const
+        {
+            return data_;
+        }
+
+        char *data()
+        {
+            return data_;
+        }
+
+        char *begin()
+        {
+            return data_;
+        }
+
+        const char *begin() const
+        {
+            return data_;
+        }
+
+        char *end()
+        {
+            return data_ + size_;
+        }
+
+        const char *end() const
+        {
+            return data_ + size_;
+        }
+
+        bool is_empty() const
+        {
+            return size_ == 0;
+        }
+
+    private:
+        char *data_;
+        int size_;
+        int capacity_;
     };
+
+    using String = Sequence<char, dyn, dyn>;
 
     String join(const String &delimeter, const Vector<String> &strings)
     {
