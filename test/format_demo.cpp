42851717d49f22c06e80648e443d236466559f56
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-11-21 01:02:37 +0900

    chore: Remove some C++ headers

--- test/format_demo.cpp
+++ test/format_demo.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include "efp.hpp"
 
-// #include <cassert>
-// #include <cstdio>
-// #include <cstring>
-
 using namespace efp;
 
 int main()
@@ -15,19 +11,18 @@ int main()
     print("s: {s}\n", "This is string");
 
     const String str0 = "This is 42";
-    std::cout << str0 << std::endl;
     // print(str0);
+    std::cout << str0 << std::endl;
 
     const auto str = format("This is {}", 42);
-    std::cout << str << std::endl;
     // print(str);
+    std::cout << str << std::endl;
 
     const auto fmt0 = auto_format("a: {}", a);
     const auto fmt1 = auto_format("s: {s}", "This is string");
 
     // print(fmt0);
     // print(fmt1);
-
     std::cout << fmt0 << std::endl;
     std::cout << fmt1 << std::endl;
 
