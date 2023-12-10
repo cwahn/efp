336ac32b0d4ca8977559b49642b985f4b689e1a4
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-12-10 22:06:51 +0900

    chore: Clean static assertion for sequence trait

--- test/sequence_test.hpp
+++ test/sequence_test.hpp
@@ -11,30 +11,11 @@ static_assert(IsSequence<Array<int, 3>>::value, "Array<int, 3> should satisfy Is
 static_assert(ImplTypeLevelFunction<Element, Array<int, 3>>::value, "Element trait should be satisfied by Array");
 static_assert(ImplTypeLevelFunction<CtSize, Array<int, 3>>::value, "CtSize trait should be satisfied by Array");
 static_assert(ImplTypeLevelFunction<CtCapacity, Array<int, 3>>::value, "CtCapacity trait should be satisfied by Array");
-
 static_assert(IsSequenceImplLength<Array<int, 3>>::value, "IsSequenceImplLength should be satisfied by Array");
-// static_assert(IsSequenceImplNth<Array<int, 3>>::value, "IsSequenceImplNth should be satisfied by Array");
-// static_assert(IsSequenceImplData<Array<int, 3>>::value, "IsSequenceImplData should be satisfied by Array");
+static_assert(IsSequenceImplNth<Array<int, 3>>::value, "IsSequenceImplNth should be satisfied by Array");
+static_assert(IsSequenceImplData<Array<int, 3>>::value, "IsSequenceImplData should be satisfied by Array");
 
-// ! temp
-
-// static_assert(IsSequenceImplNth<Array<int, 3>>::value, "IsSequenceImplNth should be satisfied by Array");
-
-// Breaking down the checks for IsSequenceImplNth
-// static_assert(IsInvocable<decltype(nth), size_t, const Array<int, 3> &>::value, "nth function is not invocable with const Array");
-// static_assert(IsInvocable<decltype(nth), size_t, Array<int, 3> &>::value, "nth function is not invocable with non-const Array");
-
-static_assert(IsSame<decltype(nth(0, std::declval<const Array<int, 3> &>())), const int &>::value, "nth for const Array does not return const int&");
-static_assert(IsSame<decltype(nth(0, std::declval<Array<int, 3> &>())), int &>::value, "nth for non-const Array does not return int&");
-
-// static_assert(IsSequenceImplData<Array<int, 3>>::value, "IsSequenceImplData should be satisfied by Array");
-
-// Breaking down the checks for IsSequenceImplData
-// static_assert(IsInvocable<decltype(data), const Array<int, 3> &>::value, "data function is not invocable with const Array");
-// static_assert(IsInvocable<decltype(data), Array<int, 3> &>::value, "data function is not invocable with non-const Array");
-
-static_assert(IsSame<decltype(data(std::declval<const Array<int, 3> &>())), const int *>::value, "data for const Array does not return const int*");
-static_assert(IsSame<decltype(data(std::declval<Array<int, 3> &>())), int *>::value, "data for non-const Array does not return int*");
+// todo Add assertion for other Containers...
 
 TEST_CASE("Initialization")
 {
