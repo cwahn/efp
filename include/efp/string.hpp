10bf43a5b36dfc8a450c7a35a96fc44b0af4161d
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-02-02 01:21:28 +0900

    refactor: Make String to inherit  from detail::VectorBase<char>

--- include/efp/string.hpp
+++ include/efp/string.hpp
@@ -9,122 +9,72 @@ namespace efp {
 // Owned C-style null operator including const char*
 // Not safe to build from regular const char *
 class CString {
-  private:
-    const char* ptr_;
-
   public:
     // const char pointer with nullptr.
-    CString(const char* ptr) : ptr_(ptr) {}
+    CString(const char* ptr) : _data(ptr) {}
 
     CString(const CString&) = delete;
     CString& operator=(const CString&) = delete;
 
-    CString(CString&& other) noexcept : ptr_(other.ptr_) {
-        other.ptr_ = nullptr;
+    CString(CString&& other) noexcept : _data(other._data) {
+        other._data = nullptr;
     }
 
     CString& operator=(CString&& other) noexcept {
         if (this != &other) {
-            delete[] ptr_;
+            delete[] _data;
 
-            ptr_ = other.ptr_;
-            other.ptr_ = nullptr;
+            _data = other._data;
+            other._data = nullptr;
         }
         return *this;
     }
 
     ~CString() {
-        delete[] ptr_;
+        delete[] _data;
     }
 
     operator const char*() const {
-        return ptr_;
+        return _data;
     }
 
     // Equality operator for comparing with another CString
     bool operator==(const CString& other) const {
         // If both are null, they are considered equal
-        if (ptr_ == nullptr && other.ptr_ == nullptr)
+        if (_data == nullptr && other._data == nullptr)
             return true;
 
         // If one is null and the other is not, they can't be equal
-        if (ptr_ == nullptr || other.ptr_ == nullptr)
+        if (_data == nullptr || other._data == nullptr)
             return false;
 
         // Use strcmp to compare the strings
-        return strcmp(ptr_, other.ptr_) == 0;
+        return strcmp(_data, other._data) == 0;
     }
 
     // Equality operator for comparing with a C-style string
     bool operator==(const char* c_str) const {
         // If both are null, they are considered equal
-        if (ptr_ == nullptr && c_str == nullptr)
+        if (_data == nullptr && c_str == nullptr)
             return true;
 
         // If one is null and the other is not, they can't be equal
-        if (ptr_ == nullptr || c_str == nullptr)
+        if (_data == nullptr || c_str == nullptr)
             return false;
 
         // Use strcmp to compare the strings
-        return strcmp(ptr_, c_str) == 0;
+        return strcmp(_data, c_str) == 0;
     }
+
+  private:
+    const char* _data;
 };
 
 template<>
-// class Sequence<char, dyn, dyn>
 class Vector<char>: public detail::VectorBase<char> {
   public:
     using Base = detail::VectorBase<char>;
     using Base::Base;
-
-    // using Element = char;
-    // static constexpr size_t ct_size = dyn;
-    // static constexpr size_t ct_capacity = dyn;
-
-    // static_assert(ct_size >= -1, "ct_size must greater or equal than -1.");
-    // static_assert(ct_capacity >= -1, "ct_capacity must greater or equal than -1.");
-
-    // Vector() : _data {nullptr}, _size {0}, _capacity {0} {}
-
-    // Vector(const Vector& other) : _data {nullptr}, _size {0}, _capacity {0} {
-    //     if (other._data) {
-    //         _size = other._size;
-    //         _capacity = other._capacity;
-    //         _data = new char[_capacity];
-
-    //         // memcpy(_data, other._data, sizeof(char) * _size);
-    //         for (size_t i = 0; i < other._size; ++i) {
-    //             new (&_data[i]) char(other._data[i]);
-    //         }
-    //     }
-    // }
-
-    // Vector(Vector&& other) : _data {other._data}, _size {other._size}, _capacity {other._capacity} {
-    //     other._data = nullptr;
-    //     other._size = 0;
-    //     other._capacity = 0;
-    // }
-
-    // ! Varadic constructor is not working for String
-    // template <typename... Args>
-    // Sequence(const Args &...args)
-    //     : _data{new char[sizeof...(args)]},
-    //       _capacity(sizeof...(args)),
-    //       _size(sizeof...(args))
-    // {
-    //     size_t i = 0;
-    //     for (auto arg : std::initializer_list<char>{args...})
-    //         _data[i++] = arg;
-    // }
-
-    // Vector(const char* c_str)
-    //     : _size {strlen(c_str)}, _capacity {_size},
-    //       _data {static_cast<char*>(::operator new[](_capacity * sizeof(char)))} {
-    //     // for (size_t i = 0; c_str[i] != '\0'; ++i) {
-    //     //     push_back(c_str[i]);
-    //     // }
-    //     memcpy(_data, c_str, _size);
-    // }
 
     Vector(const char* c_str) {
         _capacity = strlen(c_str);
@@ -132,87 +82,6 @@ class Vector<char>: public detail::VectorBase<char> {
         _size = _capacity;
         memcpy(_data, c_str, _size);
     }
-
-    // ~Vector() {
-    //     if (_data) {
-    //         delete[] _data;
-    //         _data = nullptr;
-    //     }
-    // }
-
-    // // todo copy_and_swap
-    // Vector& operator=(const Vector& other) noexcept {
-    //     if (this != &other) {
-    //         char* newData = nullptr;
-    //         if (other._size > 0) {
-    //             newData = new char[other._capacity];
-
-    //             for (size_t i = 0; i < other._size; ++i) {
-    //                 newData[i] = other._data[i];
-    //             }
-    //         }
-
-    //         _data = newData;
-    //         _size = other._size;
-    //         _capacity = other._capacity;
-    //     }
-    //     return *this;
-    // }
-
-    // Vector& operator=(Vector&& other) noexcept {
-    //     if (this != &other) {
-    //         delete[] _data;
-
-    //         _data = other._data;
-    //         _size = other._size;
-    //         _capacity = other._capacity;
-
-    //         other._data = nullptr;
-    //         other._size = 0;
-    //         other._capacity = 0;
-    //     }
-    //     return *this;
-    // }
-
-    // Vector assign_impl(const Vector& other) {
-    //     if (this != &other) {
-    //         char* newData = nullptr;
-    //         if (other._size > 0) {
-    //             newData = new char[other._capacity];
-
-    //             for (size_t i = 0; i < other._size; ++i) {
-    //                 newData[i] = other._data[i];
-    //             }
-    //         }
-
-    //         _data = newData;
-    //         _size = other._size;
-    //         _capacity = other._capacity;
-    //     }
-    //     return *this;
-    // }
-
-    // char& operator[](int index) {
-    //     return _data[index];
-    // }
-
-    // const char& operator[](int index) const {
-    //     return _data[index];
-    // }
-
-    // bool operator==(const Vector& other) const {
-    //     if (_size != other._size) {
-    //         return false;
-    //     }
-
-    //     for (size_t i = 0; i < _size; ++i) {
-    //         if (_data[i] != other._data[i]) {
-    //             return false;
-    //         }
-    //     }
-
-    //     return true;
-    // }
 
     bool operator==(const Vector& other) const {
         return Base::operator==(other);
@@ -236,71 +105,12 @@ class Vector<char>: public detail::VectorBase<char> {
         return c_str[_size] == '\0';
     }
 
-    // size_t size() const {
-    //     return _size;
-    // }
+    // todo STL only
+    operator std::string() const {
+        return std::string(_data, _size);
+    }
 
-    // size_t capacity() const {
-    //     return _capacity;
-    // }
-
-    // void resize(int length) {
-    //     if (length < 0) {
-    //         throw std::runtime_error("length must be greater or equal than 0.");
-    //     }
-
-    //     if (length > _capacity) {
-    //         reserve(length);
-    //     }
-
-    //     _size = length;
-    // }
-
-    // void reserve(int new_capacity) {
-    //     if (new_capacity > _capacity) {
-    //         char* new_data = new char[new_capacity];
-
-    //         for (size_t i = 0; i < _size; ++i) {
-    //             new_data[i] = _data[i];
-    //         }
-
-    //         delete[] _data;
-
-    //         _data = new_data;
-    //         _capacity = new_capacity;
-    //     }
-    // }
-
-    // void push_back(const char& value) {
-    //     if (_size >= _capacity) {
-    //         reserve(_capacity == 0 ? 1 : 2 * _capacity);
-    //     }
-
-    //     new (&_data[_size]) char {value};
-    //     ++_size;
-    // }
-
-    // void push_back(char&& value) {
-    //     if (_size >= _capacity) {
-    //         reserve(_capacity == 0 ? 1 : 2 * _capacity);
-    //     }
-
-    //     new (&_data[_size]) char(efp::move(value));
-    //     ++_size;
-    // }
-
-    // void erase(int index) {
-    //     if (index < 0 || index >= _size) {
-    //         throw std::runtime_error("Vector<char>::erase: index out of range");
-    //     }
-
-    //     for (size_t i = index; i < _size - 1; ++i) {
-    //         new (&_data[i]) char(efp::move(_data[i + 1]));
-    //     }
-
-    //     --_size;
-    // }
-
+    // todo Change to hold one extra capacity for null character
     const CString c_str() const {
         const size_t _size = size();
         char* extended_buffer = new char[_size + 1];
@@ -315,43 +125,9 @@ class Vector<char>: public detail::VectorBase<char> {
     // }
 
     // todo Interface with StringView
-
     void append_mut(const Vector& string) {
         for_each([&](char c) { push_back(c); }, string);
     }
-
-    // const char* data() const {
-    //     return _data;
-    // }
-
-    // char* data() {
-    //     return _data;
-    // }
-
-    // char* begin() {
-    //     return _data;
-    // }
-
-    // const char* begin() const {
-    //     return _data;
-    // }
-
-    // char* end() {
-    //     return _data + _size;
-    // }
-
-    // const char* end() const {
-    //     return _data + _size;
-    // }
-
-    // bool empty() const {
-    //     return _size == 0;
-    // }
-
-    //   private:
-    //     char* _data;
-    //     size_t _size;
-    //     size_t _capacity;
 };
 
 // using String = Sequence<char, dyn, dyn>;
