2b035ae447f5e86e2025dc3d701fef5d70011983
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-02-15 21:46:49 +0900

    chore: Add freestanding headers per ISO standard

--- include/efp/cpp_core.hpp
+++ include/efp/cpp_core.hpp
@@ -1,24 +1,38 @@
 #ifndef CPP_CORE_HPP
 #define CPP_CORE_HPP
 
-// C cores
-#include <float.h>  // Macros for numeric limits
-#include <math.h>   // Common mathematical functions (e.g., sin, cos, abs).
-#include <stdarg.h>
-#include <stddef.h>  // Common macros (e.g., NULL, int) and limit constants.
-#include <stdint.h>  // Fixed-width size_tegers (e.g., uint8_t, size_t16_t, etc.)
-#include <stdio.h>   // Input and output utilities (e.g., sprintf, snprintf).
-#include <stdlib.h>  // Standard utility functions (e.g., malloc, free, atoi, itoa).
+// C++ 11 Freestanding headers specified by N3242=11-0012
+
+#include <atomic>   // Defines atomic operations on data shared between threads.
+#include <cstdarg>  // Defines a variable argument list and macros for accessing it.
+#include <cstddef>  // Defines int, ptrdiff_t, and nullptr_t
+#include <cstdlib>  // Defines general utilities: memory management, program utilities, string conversions, random numbers, and more.
+#include <exception>  // Defines std::exception, the base class for all exceptions.
+#include <initializer_list>  // Defines std::initializer_list, which is used to create a list of elements of the same type.
+#include <limits>  // Defines numeric limits
+#include <new>  // Defines operator new and operator delete, which can be overridden for custom memory management.
+#include <thread>       // Defines classes and functions for managing threads.
+#include <type_traits>  // Defines compile-time type information.
+#include <typeinfo>     // Defines std::type_info, which is returned by typeid.
+
+// // C cores
+// #include <float.h>  // Macros for numeric limits
+// #include <math.h>   // Common mathematical functions (e.g., sin, cos, abs).
+// #include <stdarg.h>
+// #include <stddef.h>  // Common macros (e.g., NULL, int) and limit constants.
+// #include <stdint.h>  // Fixed-width size_tegers (e.g., uint8_t, size_t16_t, etc.)
+// #include <stdio.h>   // Input and output utilities (e.g., sprintf, snprintf).
+// #include <stdlib.h>  // Standard utility functions (e.g., malloc, free, atoi, itoa).
 #include <string.h>  // String handling functions (e.g., strcpy, strlen, strcat).
 
-// C++
-#include <cstddef>  // Defines int, ptrdiff_t, and nullptr_t
-#include <new>  // Defines operator new and operator delete, which can be overridden for custom memory management.
+// // C++
+// #include <cstddef>  // Defines int, ptrdiff_t, and nullptr_t
+// #include <new>  // Defines operator new and operator delete, which can be overridden for custom memory management.
 
-// ! temp
-#include <initializer_list>
-// todo STL only
-#include <array>
+// // ! temp
+// #include <initializer_list>
+// // todo STL only
+// #include <array>
 #include <iostream>
 #include <string>
 #include <vector>
