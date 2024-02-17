b0a389ae3c2c24070737b7e1ddd955fbd8f706c1
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-02-17 22:00:59 +0900

    test: Add test cases for WString

--- test/efp/string_test.hpp
+++ test/efp/string_test.hpp
@@ -8,18 +8,27 @@
 
 using namespace efp;
 
-TEST_CASE("String class functionality", "[String]") {
+TEST_CASE("BasicString<Char>", "[BasicString<Char>]") {
     SECTION("Default Constructor") {
         String str;
         CHECK(str.empty());
         CHECK(str.size() == 0);
         CHECK(str.capacity() == 0);
+
+        WString wstr;
+        CHECK(wstr.empty());
+        CHECK(wstr.size() == 0);
+        CHECK(wstr.capacity() == 0);
     }
 
     SECTION("Constructor from C-Style String") {
         String str("Hello");
         CHECK(str.size() == 5);
         CHECK(std::strcmp(str.data(), "Hello") == 0);
+
+        WString wstr(L"Hello");
+        CHECK(wstr.size() == 5);
+        CHECK(std::wcscmp(wstr.data(), L"Hello") == 0);
     }
 
     SECTION("Copy Constructor") {
@@ -27,6 +36,10 @@ TEST_CASE("String class functionality", "[String]") {
         String copy = original;
         CHECK(copy == original);
         CHECK(copy.size() == original.size());
+
+        WString woriginal(L"Hello");
+        WString wcopy = woriginal;
+        CHECK(wcopy == woriginal);
     }
 
     SECTION("Move Constructor") {
@@ -34,6 +47,11 @@ TEST_CASE("String class functionality", "[String]") {
         String moved(efp::move(original));
         CHECK(std::strcmp(moved.data(), "Hello") == 0);
         CHECK(original.empty());
+
+        WString woriginal(L"Hello");
+        WString wmoved(efp::move(woriginal));
+        CHECK(std::wcscmp(wmoved.data(), L"Hello") == 0);
+        CHECK(woriginal.empty());
     }
 
     SECTION("Copy Assignment Operator") {
@@ -42,6 +60,12 @@ TEST_CASE("String class functionality", "[String]") {
         str2 = str1;
         CHECK(str2 == str1);
         CHECK(str2.size() == str1.size());
+
+        WString wstr1(L"Hello");
+        WString wstr2;
+        wstr2 = wstr1;
+        CHECK(wstr2 == wstr1);
+        CHECK(wstr2.size() == wstr1.size());
     }
 
     SECTION("Move Assignment Operator") {
@@ -50,6 +74,12 @@ TEST_CASE("String class functionality", "[String]") {
         str2 = efp::move(str1);
         CHECK(std::strcmp(str2.data(), "Hello") == 0);
         CHECK(str1.empty());
+
+        WString wstr1(L"Hello");
+        WString wstr2;
+        wstr2 = efp::move(wstr1);
+        CHECK(std::wcscmp(wstr2.data(), L"Hello") == 0);
+        CHECK(wstr1.empty());
     }
 
     SECTION("Destruction") {
@@ -58,41 +88,59 @@ TEST_CASE("String class functionality", "[String]") {
         CHECK(true);  // If we reach this point, destruction didn't cause a crash
     }
 
-    SECTION("String::operator[]") {
+    SECTION("BasicString<Char>::operator[]") {
         String str("Hello");
         CHECK(str[0] == 'H');
         CHECK(str[4] == 'o');
+
+        WString wstr(L"Hello");
+        CHECK(wstr[0] == L'H');
+        CHECK(wstr[4] == L'o');
     }
 
-    SECTION("String::at") {
+    SECTION("BasicString<Char>::at") {
         String str("Hello");
         CHECK(str.at(0) == 'H');
         CHECK(str.at(4) == 'o');
+
+        WString wstr(L"Hello");
+        CHECK(wstr.at(0) == L'H');
+        CHECK(wstr.at(4) == L'o');
     }
 
-    SECTION("String::front") {
+    SECTION("BasicString<Char>::front") {
         String str("Hello");
         CHECK(str.front() == 'H');
+
+        WString wstr(L"Hello");
+        CHECK(wstr.front() == L'H');
     }
 
-    SECTION("String::back") {
+    SECTION("BasicString<Char>::back") {
         String str("Hello");
         CHECK(str.back() == 'o');
+
+        WString wstr(L"Hello");
+        CHECK(wstr.back() == L'o');
     }
 
-    SECTION("String::resize") {
+    SECTION("BasicString<Char>::resize") {
         String str("Hello");
         str.resize(10);
         CHECK(str.size() == 10);
+
+        WString wstr(L"Hello");
+        wstr.resize(10);
+        CHECK(wstr.size() == 10);
     }
 
-    SECTION("String::reserve") {
+    SECTION("BasicString<Char>::reserve") {
         String str("Hello");
         str.reserve(10);
         CHECK(str.capacity() >= 10);
     }
 
-    SECTION("String::push_back") {
+    SECTION("BasicString<Char>::push_back") {
         String str;
         str.push_back('H');
         str.push_back('i');
@@ -100,89 +148,146 @@ TEST_CASE("String class functionality", "[String]") {
         CHECK(std::strcmp(str.data(), "Hi") == 0);
     }
 
-    SECTION("String::pop_back") {
+    SECTION("BasicString<Char>::pop_back") {
         String str("Hello");
         str.erase(0);  // Remove 'H'
         CHECK(str.size() == 4);
         CHECK(str == "ello");
+
+        WString wstr(L"Hello");
+        wstr.erase(0);  // Remove 'H'
+        CHECK(wstr.size() == 4);
+        CHECK(wstr == L"ello");
     }
 
-    SECTION("String::c_str") {
+    SECTION("BasicString<Char>::c_str") {
         String str("Hello");
         CHECK(std::strcmp(str.c_str(), "Hello") == 0);
+
+        WString wstr(L"Hello");
+        CHECK(std::wcscmp(wstr.c_str(), L"Hello") == 0);
     }
 
-    SECTION("String::operator+=") {
+    SECTION("BasicString<Char>::operator+=") {
         String str("Hello");
         str += " World";
         CHECK(str == "Hello World");
+
+        WString wstr(L"Hello");
+        wstr += L" World";
+        CHECK(wstr == L"Hello World");
     }
 
-    SECTION("String::append(const Char*)") {
+    SECTION("BasicString<Char>::append(const Char*)") {
         String str("Hello");
         str.append(" World");
         CHECK(str == "Hello World");
+
+        WString wstr(L"Hello");
+        wstr.append(L" World");
+        CHECK(wstr == L"Hello World");
     }
 
-    SECTION("String::append(size_t, Char)") {
+    SECTION("BasicString<Char>::append(size_t, Char)") {
         String str("Hello");
         str.append(3, '!');
         CHECK(str == "Hello!!!");
+
+        WString wstr(L"Hello");
+        wstr.append(3, L'!');
+        CHECK(wstr == L"Hello!!!");
     }
 
-    SECTION("String::assign") {
+    SECTION("BasicString<Char>::assign") {
         String str("Hello");
         str.assign("World");
         CHECK(str == "World");
+
+        WString wstr(L"Hello");
+        wstr.assign(L"World");
+        CHECK(wstr == L"World");
     }
 
-    SECTION("String::insert") {
+    SECTION("BasicString<Char>::insert") {
         String str("Hello");
         str.insert(5, " World!!!", 6);
         CHECK(str == "Hello World");
+
+        WString wstr(L"Hello");
+        wstr.insert(5, L" World!!!", 6);
+        CHECK(wstr == L"Hello World");
     }
 
-    SECTION("String::substr") {
+    SECTION("BasicString<Char>::substr") {
         String str("Hello World");
         String sub = str.substr(6, 5);
         CHECK(sub == "World");
-
         sub = str.substr(0, 5);
         CHECK(sub == "Hello");
-
         sub = str.substr(6);
         CHECK(sub == "World");
+
+        WString wstr(L"Hello World");
+        WString wsub = wstr.substr(6, 5);
+        CHECK(wsub == L"World");
+        wsub = wstr.substr(0, 5);
+        CHECK(wsub == L"Hello");
+        wsub = wstr.substr(6);
+        CHECK(wsub == L"World");
     }
 
-    SECTION("String::copy") {
+    SECTION("BasicString<Char>::copy") {
         String str("Hello");
         char buffer[10];
         str.copy(buffer, 5);
         CHECK(std::strncmp(buffer, "Hello", 5) == 0);
+
+        WString wstr(L"Hello");
+        wchar_t wbuffer[10];
+        wstr.copy(wbuffer, 5);
+        CHECK(std::wcsncmp(wbuffer, L"Hello", 5) == 0);
     }
 
-    SECTION("String::compare(const String& other)") {
+    SECTION("BasicString<Char>::compare(const String& other)") {
         String str1("Hello");
         String str2("Hello");
         String str3("World");
         CHECK(str1.compare(str2) == 0);
         CHECK(str1.compare(str3) < 0);
         CHECK(str3.compare(str1) > 0);
+
+        WString wstr1(L"Hello");
+        WString wstr2(L"Hello");
+        WString wstr3(L"World");
+        CHECK(wstr1.compare(wstr2) == 0);
+        CHECK(wstr1.compare(wstr3) < 0);
+        CHECK(wstr3.compare(wstr1) > 0);
     }
 
-    SECTION("String::compare(size_t pos, size_t count, const String& other)") {
+    SECTION("BasicString<Char>::compare(size_t pos, size_t count, const String& other)") {
         String str1("Hello");
         String str2("Hello");
         String str3("World");
         CHECK(str1.compare(0, 5, str2) == 0);
         CHECK(str1.compare(0, 5, str3) < 0);
         CHECK(str3.compare(0, 5, str1) > 0);
+
+        WString wstr1(L"Hello");
+        WString wstr2(L"Hello");
+        WString wstr3(L"World");
+        CHECK(wstr1.compare(0, 5, wstr2) == 0);
+        CHECK(wstr1.compare(0, 5, wstr3) < 0);
+        CHECK(wstr3.compare(0, 5, wstr1) > 0);
     }
 
     SECTION("Equality with C-Style String") {
         String str("Hello");
         CHECK(str == "Hello");
         CHECK_FALSE(str == "Hi");
+
+        WString wstr(L"Hello");
+        CHECK(wstr == L"Hello");
+        CHECK_FALSE(wstr == L"Hi");
     }
 }
 
