299cf3ab39c9b8da09988cf17511ff4b0d4a5bf5
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-02-18 12:30:37 +0900

    feat: Make efp::Sequence are formattable with format

--- include/efp/format.hpp
+++ include/efp/format.hpp
@@ -21,24 +21,66 @@ inline void print(FormatString<Args...> fmt, Args&&... args) {
                                   : fmt::detail::vprint_mojibake(stdout, fmt, vargs);
 }
 
-template<typename... Args>
-inline void print(std::FILE* f, fmt::format_string<Args...> fmt, Args&&... args) {
-    const auto& vargs = fmt::make_format_args(args...);
-    return fmt::detail::is_utf8() ? fmt::vprint(f, fmt, vargs)
-                                  : fmt::detail::vprint_mojibake(f, fmt, vargs);
-}
+// template<typename... Args>
+// inline void print(std::FILE* f, fmt::format_string<Args...> fmt, Args&&... args) {
+//     const auto& vargs = fmt::make_format_args(args...);
+//     return fmt::detail::is_utf8() ? fmt::vprint(f, fmt, vargs)
+//                                   : fmt::detail::vprint_mojibake(f, fmt, vargs);
+// }
 
 template<typename... Args>
 inline void println(FormatString<Args...> fmt, Args&&... args) {
     return fmt::println(stdout, fmt, efp::forward<Args>(args)...);
 }
 
-template<typename... Args>
-inline void println(std::FILE* f, fmt::format_string<Args...> fmt, Args&&... args) {
-    return fmt::print(f, "{}\n", fmt::format(fmt, efp::forward<Args>(args)...));
-}
-
-// todo styled print
+// template<typename... Args>
+// inline void println(std::FILE* f, fmt::format_string<Args...> fmt, Args&&... args) {
+//     return fmt::print(f, "{}\n", fmt::format(fmt, efp::forward<Args>(args)...));
+// }
 
 }  // namespace efp
+
+// Specialize fmt::formatter for efp::Array
+template<typename T, size_t ct_size>
+struct fmt::formatter<efp::Array<T, ct_size>> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) const -> format_parse_context::iterator {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const efp::Array<T, ct_size>& arr, FormatContext& ctx) -> format_context::iterator {
+        return fmt::format_to(ctx.out(), "[{}]", fmt::join(arr.begin(), arr.end(), ", "));
+    }
+};
+
+template<typename T, size_t ct_cap>
+struct fmt::formatter<efp::ArrVec<T, ct_cap>> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) const -> format_parse_context::iterator {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const efp::ArrVec<T, ct_cap>& arrvec, FormatContext& ctx)
+        -> format_context::iterator {
+        return fmt::format_to(ctx.out(), "[{}]", fmt::join(arrvec.begin(), arrvec.end(), ", "));
+    }
+};
+
+// Do the same for efp::Vector and efp::ArrVec
+template<typename T, typename Allocator, typename Traits>
+struct fmt::formatter<efp::Vector<T, Allocator, Traits>> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) const -> format_parse_context::iterator {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const efp::Vector<T, Allocator, Traits>& vec, FormatContext& ctx)
+        -> format_context::iterator {
+        return fmt::format_to(ctx.out(), "[{}]", fmt::join(vec.begin(), vec.end(), ", "));
+    }
+};
+
 #endif
--- test/efp/format_test.hpp
+++ test/efp/format_test.hpp
@@ -9,33 +9,58 @@
 using namespace efp;
 
 TEST_CASE("format", "[format]") {
-    CHECK(format("{}", 42) == "42");
+    SECTION("fundamental types") {
+        CHECK(format("{}", 42) == "42");
 
-    CHECK(format("{}", 42.0) == "42");
-    CHECK(format("{}", 42.1) == "42.1");
+        CHECK(format("{}", 42.0) == "42");
+        CHECK(format("{}", 42.1) == "42.1");
 
-    CHECK(format("{}", "hello") == "hello");
-    CHECK(format("{}", 'c') == "c");
-    CHECK(format("{}", 42u) == "42");
-    CHECK(format("{}", 42l) == "42");
-    CHECK(format("{}", 42ll) == "42");
+        CHECK(format("{}", "hello") == "hello");
+        CHECK(format("{}", 'c') == "c");
 
-    CHECK(format("{}", 42.0f) == "42");
-    CHECK(format("{}", 42.1f) == "42.1");
+        CHECK(format("{}", 42u) == "42");
+        CHECK(format("{}", 42l) == "42");
+        CHECK(format("{}", 42ll) == "42");
 
-    CHECK(format("{}", 42.0l) == "42");
-    CHECK(format("{}", 42.1l) == "42.1");
+        CHECK(format("{}", 42.0f) == "42");
+        CHECK(format("{}", 42.1f) == "42.1");
 
-    // format specifiers
-    // CHECK(format("{:d}", 42) == "42");
-    // CHECK(format("{:f}", 42.0) == "42.000000");
-    // CHECK(format("{:s}", "hello") == "hello");
-    // CHECK(format("{:c}", 'c') == "c");
-    // // CHECK(format("{:u}", 42u) == "42");
-    // // CHECK(format("{:l}", 42l) == "42");
-    // // CHECK(format("{:ll}", 42ll) == "42");
-    // CHECK(format("{:f}", 42.0f) == "42.000000");
-    // CHECK(format("{:f}", 42.0l) == "42.000000");
+        CHECK(format("{}", 42.0l) == "42");
+        CHECK(format("{}", 42.1l) == "42.1");
+
+        // format specifiers
+        CHECK(format("{:d}", 42) == "42");
+        CHECK(format("{:f}", 42.0) == "42.000000");
+        CHECK(format("{:f}", 42.1) == "42.100000");
+        CHECK(format("{:s}", "hello") == "hello");
+        CHECK(format("{:c}", 'c') == "c");
+        // CHECK(format("{:u}", 42u) == "42");
+        // CHECK(format("{:l}", 42l) == "42");
+        // CHECK(format("{:ll}", 42ll) == "42");
+        CHECK(format("{:f}", 42.0f) == "42.000000");
+        CHECK(format("{:f}", 42.1f) == "42.099998");
+        CHECK(format("{:f}", 42.0l) == "42.000000");
+        CHECK(format("{:f}", 42.1l) == "42.100000");
+    }
+
+    SECTION("efp::Array") {
+        CHECK(format("{}", array_3) == "[1, 2, 3]");
+        CHECK(format("{}", array_5) == "[1, 2, 3, 4, 5]");
+    }
+
+    SECTION("efp::ArrVec") {
+        CHECK(format("{}", arrvec_3) == "[1, 2, 3]");
+        CHECK(format("{}", arrvec_5) == "[1, 2, 3, 4, 5]");
+    }
+
+    SECTION("efp::Vector") {
+        CHECK(format("{}", vector_3) == "[1, 2, 3]");
+        CHECK(format("{}", vector_5) == "[1, 2, 3, 4, 5]");
+    }
+
+    SECTION("efp::String") {
+        CHECK(format("{}", String("hello")) == "hello");
+    }
 }
 
 #endif
